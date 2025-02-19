87ef7fecbb640be05993e30b1f2a3d4dd4d6c8ff
hungerbk <wjantlr@gmail.com>
2025-02-19 23:38:05 +0900

    [Refactor] 단어 정렬 / 실버 5

--- CLASS 2+/1181번： 단어 정렬/단어 정렬.py
+++ CLASS 2+/1181번： 단어 정렬/단어 정렬.py
@@ -1,15 +1,31 @@
+# import sys
+
+# def input():
+#   return sys.stdin.readline().rstrip()
+
+# n = int(input())
+# word_set = set()
+
+# for i in range(n):
+#   word_set.add(input())
+
+# sorted_list = sorted(word_set)
+
+# for i in sorted(sorted_list, key= lambda x: len(x)):
+#   print(i)
+
 import sys
 
 def input():
   return sys.stdin.readline().rstrip()
 
 n = int(input())
-word_set = set()
+answer = set()
 
-for i in range(n):
-  word_set.add(input())
+for _ in range(n):
+  answer.add(input())
 
-sorted_list = sorted(word_set)
+answer = sorted(answer, key=lambda x: (len(x), x))
 
-for i in sorted(sorted_list, key= lambda x: len(x)):
+for i in answer:
   print(i)

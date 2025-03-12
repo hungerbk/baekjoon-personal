ce24123c35304878f74976e4c2601aeeacec6309
hungerbk <wjantlr@gmail.com>
2025-03-12 23:58:38 +0900

    [Refactor] 최대공약수와 최소공배수 / 브론즈 1 / 5분 / X

--- CLASS 2+/2609번： 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ CLASS 2+/2609번： 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -5,5 +5,18 @@ def input():
 
 n, m = map(int, input().split())
 
+# 파이썬 내장 함수
 print(math.gcd(n, m))
 print(math.lcm(n, m))
+
+# 유클리드 호제법
+def gcd(n, m):
+    while m > 0:
+        n, m = m, n % m
+    return n
+
+def lcm(n, m):
+    return n * m // gcd(n, m)
+
+print(gcd(n, m))
+print(lcm(n, m))

c9e6dc91de1b43083ccb5b405a02d71903e18546
hungerbk <wjantlr@gmail.com>
2025-04-19 02:10:06 +0900

    문자열 분석 / 실버 2 / X / 10분

--- GPT/10820번: 문자열 분석/문자열 분석.py
+++ GPT/10820번: 문자열 분석/문자열 분석.py
@@ -1,11 +1,31 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 10820                             :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: kbk0424 <boj.kr/u/kbk0424>                  +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/10820                          #+#        #+#      #+#     #
-#    Solved: 2025/04/19 00:38:55 by kbk0424       ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+# def input():
+#   return sys.stdin.readline().rstrip()
+
+# # 소문자, 대문자, 숫자, 공백 순서
+# string = input()
+# answer = [0] * 4
+# for i in string:
+#     if i.islower():
+#         answer[0] += 1
+#     elif i.isupper():
+#         answer[1] += 1
+#     elif i.isdigit():
+#         answer[2] += 1
+#     else:
+#         answer[3] += 1
+# print(' '.join(map(str, answer)))
+
+for line in sys.stdin:
+    answer = [0, 0, 0, -1]
+    for i in line:
+        if i.islower():
+            answer[0] += 1
+        elif i.isupper():
+            answer[1] += 1
+        elif i.isdigit():
+            answer[2] += 1
+        else:
+            answer[3] += 1
+    print(' '.join(map(str, answer)))

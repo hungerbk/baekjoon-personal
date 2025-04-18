efe8738ad50201ee890e7f29606e6b052d6beb0a
hungerbk <wjantlr@gmail.com>
2025-04-19 01:49:32 +0900

    괄호 / 실버 2 / O / 10분

--- GPT/9012번: 괄호/괄호.py
+++ GPT/9012번: 괄호/괄호.py
@@ -1,11 +1,24 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 9012                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: kbk0424 <boj.kr/u/kbk0424>                  +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/9012                           #+#        #+#      #+#     #
-#    Solved: 2025/04/19 00:39:58 by kbk0424       ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def input():
+  return sys.stdin.readline().rstrip()
+
+n = int(input())
+
+for _ in range(n):
+    str = input()
+    stack = []
+    for i in str:
+        if i == '(':
+            stack.append('(')
+        else:
+            if len(stack) == 0:
+                print('NO')
+                break
+            else:
+                stack.pop()
+    else:
+        if len(stack) == 0:
+            print('YES')
+        else:
+            print('NO')

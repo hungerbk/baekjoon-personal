9661b5cf272c0516752b786e995af2fa22a19884
hungerbk <wjantlr@gmail.com>
2025-04-19 02:23:04 +0900

    스택 수열 / 실버 2 / X / 20분

--- GPT/1874번: 스택 수열/스택 수열.py
+++ GPT/1874번: 스택 수열/스택 수열.py
@@ -1,11 +1,28 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 1874                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: kbk0424 <boj.kr/u/kbk0424>                  +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/1874                           #+#        #+#      #+#     #
-#    Solved: 2025/04/19 00:40:13 by kbk0424       ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+sequence = [int(input()) for _ in range(n)]
+
+stack = []
+result = []
+current = 1  # 1부터 시작
+
+for num in sequence:
+    # 필요한 숫자까지 push
+    while current <= num:
+        stack.append(current)
+        result.append('+')
+        current += 1
+
+    # 원하는 숫자가 스택 맨 위에 있으면 pop
+    if stack[-1] == num:
+        stack.pop()
+        result.append('-')
+    else:
+    # 꺼내야 하는 숫자가 스택에 없으면 불가능
+        print("NO")
+        exit()
+
+# 성공적으로 수열을 만들었으면 출력
+print('\n'.join(result))

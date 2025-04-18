e8d52522a5411c2b54d28fe975e8575eb994ee62
hungerbk <wjantlr@gmail.com>
2025-04-19 02:28:41 +0900

    덩치 / 실버 5 / X / 10분

--- GPT/7568번: 덩치/덩치.py
+++ GPT/7568번: 덩치/덩치.py
@@ -1,11 +1,21 @@
-#  **************************************************************************  #
-#                                                                              #
-#                                                       :::    :::    :::      #
-#    Problem Number: 7568                              :+:    :+:      :+:     #
-#                                                     +:+    +:+        +:+    #
-#    By: kbk0424 <boj.kr/u/kbk0424>                  +#+    +#+          +#+   #
-#                                                   +#+      +#+        +#+    #
-#    https://boj.kr/7568                           #+#        #+#      #+#     #
-#    Solved: 2025/04/19 00:39:45 by kbk0424       ###          ###   ##.kr     #
-#                                                                              #
-#  **************************************************************************  #
+import sys
+
+def input():
+  return sys.stdin.readline().rstrip()
+
+n = int(input())
+people = [list(map(int, input().split())) for _ in range(n)]
+
+ranks = []
+
+for i in range(n):
+    count = 0
+    for j in range(n):
+        if i == j:
+            continue
+        # j가 i보다 덩치가 더 크면
+        if people[j][0] > people[i][0] and people[j][1] > people[i][1]:
+            count += 1
+    ranks.append(count + 1)
+
+print(' '.join(map(str, ranks)))
